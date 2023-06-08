ebbafea503b7dc431e9b574a299771d0905a9690
boburjon <boburr375@gmail.com>
2023-06-08 10:36:26 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/shopuitask/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shopuitask
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+   final String id = "home_page";
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> _listItem = [
+    'assets/images/image_1.jpeg',
+    'assets/images/image_2.jpg',
+    'assets/images/image_3.png',
+    'assets/images/image_4.jpg',
+    'assets/images/image_5.jpg',
+    'assets/images/image_1.jpeg',
+    'assets/images/image_2.jpg',
+    'assets/images/image_3.png',
+    'assets/images/image_4.jpg',
+    'assets/images/image_5.jpg',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.orange[900],
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          leading: const Icon(Icons.menu),
+          title: const Text("Apple Products"),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Container(
+                width: 36,
+                height: 30,
+                decoration: BoxDecoration(
+                    color: Colors.yellow[800],
+                    borderRadius: BorderRadius.circular(10)),
+                child: const Center(
+                  child: Text("7"),
+                ),
+              ),
+            ),
+          ],
+        ),
+        body: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                // #header
+                Container(
+                  width: double.infinity,
+                  height: 240,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: const DecorationImage(
+                        image: AssetImage("assets/images/image_4.jpg"),
+                        fit: BoxFit.cover),
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient:
+                      LinearGradient(begin: Alignment.bottomRight, colors: [
+                        Colors.black.withOpacity(.3),
+                        Colors.black.withOpacity(.01),
+                      ]),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        const Text(
+                          "Lifestyle sale",
+                          style: TextStyle(color: Colors.white, fontSize: 35),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        Container(
+                          height: 50,
+                          margin: const EdgeInsets.symmetric(horizontal: 40),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white),
+                          child: Center(
+                            child: Text(
+                              "Shop Now",
+                              style: TextStyle(color: Colors.grey[900]),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                // GridView
+                Expanded(
+                  child: GridView.count(
+                    crossAxisCount: 1,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    children:
+                    _listItem.map((item) => cellOfList(item)).toList(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  Widget cellOfList(String item) {
+    return Card(
+      color: Colors.transparent,
+      elevation: 0,
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(
+            image: AssetImage(item),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Icon(
+              Icons.favorite,
+              color: Colors.red,
+              size: 35,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
